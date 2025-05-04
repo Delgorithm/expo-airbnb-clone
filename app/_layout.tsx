c1bc3f2db5a6f8c647f581db60098456569c07ee
Delgorithm <delgorithm@gmail.com>
2025-05-04 16:27:43 +0200

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,6 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import "react-native-reanimated";
 import { useEffect } from "react";
-
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     mon: require("../assets/fonts/Montserrat-Regular.ttf"),
