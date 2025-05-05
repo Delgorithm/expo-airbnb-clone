51eb30a85e071716c424513f024768b7f9f950a5
Delgorithm <delgorithm@gmail.com>
2025-05-05 14:16:18 +0200

    dev

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,64 @@
+import { useCallback, useRef, useState } from "react";
 import { Text, View } from "react-native";
+import {
+  LegendList,
+  LegendListRef,
+  LegendListRenderItemProps,
+} from "@legendapp/list";
+import listings from "@/assets/data/listings.json";
+import LegendListCard from "@/components/card";
+
+const PAGE_SIZE = 10;
 
 export default function Page() {
+  const listRef = useRef<LegendListRef>(null);
+  const [page, setPage] = useState(1);
+  const [visibleData, setVisibleData] = useState(listings.slice(0, PAGE_SIZE));
+
+  const loadMore = useCallback(() => {
+    const nextPage = page + 1;
+    const start = (nextPage - 1) * PAGE_SIZE;
+    const end = start + PAGE_SIZE;
+    const nextItems = listings.slice(start, end);
+
+    if (nextItems.length > 0) {
+      setVisibleData((prev) => [...prev, ...nextItems]);
+      setPage(nextPage);
+    }
+  }, [page]);
+
+  const renderItem = ({
+    item,
+  }: LegendListRenderItemProps<(typeof listings)[0]>) => {
+    return (
+      <LegendListCard
+        title={item.title}
+        city={item.city}
+        country={item.country}
+        price={item.price}
+        rating={item.rating}
+        image={item.image}
+      />
+    );
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Index Tab Page</Text>
+    <View
+      style={{
+        flex: 1,
+        paddingTop: 48,
+      }}
+    >
+      <LegendList
+        data={listings}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id.toString()}
+        recycleItems={true}
+        maintainVisibleContentPosition
+        ref={listRef}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.5}
+      />
     </View>
   );
 }
---
+++ components/card.tsx
@@ -0,0 +1,80 @@
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import { Image } from "expo-image";
+
+interface LegendListCardProps {
+  title: string;
+  city: string;
+  country: string;
+  price: number;
+  rating: number;
+  image: string;
+}
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+export default function LegendListCard({
+  title,
+  city,
+  country,
+  price,
+  rating,
+  image,
+}: LegendListCardProps) {
+  return (
+    <View
+      style={{
+        width: "90%",
+        height: 430,
+        alignSelf: "center",
+        marginVertical: 26,
+      }}
+    >
+      <Image
+        source={{ uri: image }}
+        placeholder={{ blurhash }}
+        contentFit="cover"
+        transition={1000}
+        style={{
+          height: 320,
+          borderRadius: 16,
+        }}
+      />
+      <View style={{ marginTop: 12 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignContent: "flex-start",
+          }}
+        >
+          <View style={{ gap: 6 }}>
+            <Text style={{ fontWeight: 500 }}>
+              {title}, {city}
+            </Text>
+            <View style={{ gap: 2 }}>
+              <Text style={{ fontWeight: 200 }}>
+                Entire rental unit in Paris, {country}
+              </Text>
+              <Text style={{ fontWeight: 200 }}>4 beds</Text>
+            </View>
+
+            <View style={{ flexDirection: "row", gap: 4, marginTop: 6 }}>
+              <Text style={{ fontWeight: 500 }}>{price}</Text>
+              <Text style={{ fontWeight: 300 }}>total before taxes</Text>
+            </View>
+          </View>
+
+          <View
+            style={{ flexDirection: "row", gap: 4, alignItems: "baseline" }}
+          >
+            <Ionicons name="star" />
+            <Text>{rating}</Text>
+            <Text>(69)</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
