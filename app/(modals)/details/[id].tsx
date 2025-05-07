c8c2342bf312e9dd3354262a028d4e4d6a7c7801
Delgorithm <delgorithm@gmail.com>
2025-05-07 15:40:07 +0200

    fix : use of components

--- app/(modals)/details/[id].tsx
+++ app/(modals)/details/[id].tsx
@@ -1,12 +1,5 @@
 import { router, useLocalSearchParams } from "expo-router";
-import {
-  Dimensions,
-  Image,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Dimensions, Pressable, StyleSheet, Text, View } from "react-native";
 import listings from "@/assets/data/listings.json";
 import Animated, {
   interpolate,
@@ -14,19 +7,19 @@ import Animated, {
   useAnimatedStyle,
   useScrollViewOffset,
 } from "react-native-reanimated";
-import {
-  FontAwesome5,
-  Ionicons,
-  MaterialCommunityIcons,
-} from "@expo/vector-icons";
-import { Colors } from "@/constants/Colors";
-import AppleGoogleMap from "@/components/apple-google-map";
-import { LinearGradient } from "expo-linear-gradient";
+import { Ionicons } from "@expo/vector-icons";
+import GuestFavorite from "@/components/ui/guest-favorite";
+import DisplayMap from "@/components/ui/display-map";
+import WhatItOffers from "@/components/ui/what-it-offers";
+import FirstBadge from "@/components/ui/first-badge";
+import WhereYouSleep from "@/components/ui/where-you-sleep";
+import Informations from "@/components/ui/informations";
+import SecondBadge from "@/components/ui/second-badge";
+import LocationHouse from "@/components/ui/location-house";
+import FooterBtn from "@/components/ui/footer-btn";
 
 const { width } = Dimensions.get("window");
 const IMG_HEIGHT = 300;
-const blurhash =
-  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
 export default function DetailsPage() {
   const { id } = useLocalSearchParams();
@@ -34,9 +27,6 @@ export default function DetailsPage() {
 
   const scrollRef = useAnimatedRef<Animated.ScrollView>();
   const scrollOffset = useScrollViewOffset(scrollRef);
-
-  const fullStars = Math.floor(listing.rating);
-  const hasHalfStar = listing.rating % 1 >= 0.5;
 
   const imageAnimatedStyle = useAnimatedStyle(() => {
     return {
@@ -84,7 +74,7 @@ export default function DetailsPage() {
             style={{ padding: 8, borderRadius: 999, backgroundColor: "white" }}
           />
         </Pressable>
-        <View style={styles.viewMoreButtons}>
+        <View>
           <Pressable
             onPress={() => console.log("Share")}
             style={{ position: "absolute", top: 56, right: 80, zIndex: 999 }}
@@ -120,200 +110,24 @@ export default function DetailsPage() {
       </View>
 
       {/* Section main content */}
-      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
+      <Animated.ScrollView
+        ref={scrollRef}
+        scrollEventThrottle={16}
+        contentContainerStyle={{ paddingBottom: 600 }}
+      >
         <Animated.Image
           source={{ uri: listing.image }}
           style={[styles.image, imageAnimatedStyle]}
         />
-        <View style={{ padding: 24 }}>
-          <Text style={{ fontSize: 36, fontWeight: 500 }}>{listing.title}</Text>
-          <Text style={styles.location}>
-            {listing.city}, {listing.country}
-          </Text>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-around",
-            height: 80,
-            marginHorizontal: 24,
-            borderRadius: 10,
-            borderWidth: 0.5,
-            borderColor: "gray",
-          }}
-        >
-          {/* First element */}
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontSize: 20 }}>{listing.rating}</Text>
-            <View style={{ flexDirection: "row", gap: 4 }}>
-              {Array.from({ length: fullStars }).map((_, index) => (
-                <Ionicons key={index} name="star" size={8} color="black" />
-              ))}
-              {hasHalfStar && (
-                <Ionicons name="star-half-outline" size={8} color="black" />
-              )}
-            </View>
-          </View>
 
-          <View
-            style={{ height: "50%", width: 0.5, backgroundColor: "gray" }}
-          />
+        <LocationHouse listing={listing} />
 
-          {/* Second Element */}
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-            }}
-          >
-            <FontAwesome5
-              name="leaf"
-              size={20}
-              color="black"
-              style={{ transform: [{ scaleX: -1 }] }}
-            />
+        <FirstBadge listing={listing} />
 
-            <Text style={{ fontSize: 20, fontWeight: 500 }}>Top</Text>
-
-            <FontAwesome5 name="leaf" size={20} color="black" />
-          </View>
-
-          <View
-            style={{
-              height: "50%",
-              width: 0.5,
-              backgroundColor: "gray",
-            }}
-          />
-
-          {/* Third Elements */}
-          <View
-            style={{
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <Text style={{ fontSize: 20, fontWeight: 500 }}>
-              {listing.reviews}
-            </Text>
-            <Text style={{ textDecorationLine: "underline" }}>Avis</Text>
-          </View>
-        </View>
         {/* Informations & badges */}
-        <View
-          style={{
-            marginHorizontal: 32,
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 20,
-            marginVertical: 20,
-          }}
-        >
-          <Ionicons name="diamond-outline" size={34} color={Colors.primary} />
-          <View style={{ flexDirection: "column", gap: 6 }}>
-            <Text style={{ fontSize: 20, fontWeight: 400 }}>Gemme rare</Text>
-            <Text style={{ fontWeight: 200 }}>
-              Habituellement plein à craquer
-            </Text>
-          </View>
-        </View>
-        <View
-          style={{
-            height: 0.5,
-            marginHorizontal: 24,
-            backgroundColor: "gray",
-          }}
-        />
-        <View
-          style={{
-            marginHorizontal: 32,
-            flexDirection: "row",
-            alignItems: "center",
-            gap: 20,
-            marginVertical: 20,
-          }}
-        >
-          <FontAwesome5 name="user-alt" size={34} color={Colors.primary} />
-          <View style={{ flexDirection: "column", gap: 6 }}>
-            <Text style={{ fontSize: 20, fontWeight: 400 }}>
-              Proposé par Delgorithm
-            </Text>
-            <Text style={{ fontWeight: 200 }}>Superhost - 3 ans</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            height: 0.5,
-            marginHorizontal: 24,
-            backgroundColor: "gray",
-          }}
-        />
-        <View>
-          <View
-            style={{
-              marginHorizontal: 32,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 20,
-              marginVertical: 20,
-            }}
-          >
-            <Ionicons name="desktop-outline" size={34} color={Colors.dark} />
-            <View style={{ flexDirection: "column", gap: 6 }}>
-              <Text style={{ fontSize: 20, fontWeight: 400 }}>
-                Bureau individuel
-              </Text>
-              <Text style={{ fontWeight: 200 }}>
-                Un espace de travail rien que pour vous.
-              </Text>
-            </View>
-          </View>
 
-          <View
-            style={{
-              marginHorizontal: 32,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 20,
-              marginVertical: 20,
-            }}
-          >
-            <FontAwesome5 name="door-open" size={34} color={Colors.dark} />
-            <View style={{ flexDirection: "column", gap: 6 }}>
-              <Text style={{ fontSize: 20, fontWeight: 400 }}>
-                Proposé par Delgorithm
-              </Text>
-              <Text style={{ fontWeight: 200 }}>Vérification à l'entrée.</Text>
-            </View>
-          </View>
+        <SecondBadge />
 
-          <View
-            style={{
-              marginHorizontal: 32,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 20,
-              marginVertical: 20,
-            }}
-          >
-            <FontAwesome5 name="medal" size={34} color={Colors.dark} />
-            <View style={{ flexDirection: "column", gap: 6 }}>
-              <Text style={{ fontSize: 20, fontWeight: 400 }}>
-                Delgorithm est un Suport Hote
-              </Text>
-              <Text style={{ fontWeight: 200 }}>
-                Les Super Hote sont expérimentés et grandement recommandés.
-              </Text>
-            </View>
-          </View>
-        </View>
         <View
           style={{
             height: 0.5,
@@ -321,6 +135,9 @@ export default function DetailsPage() {
             backgroundColor: "gray",
           }}
         />
+
+        <Informations />
+
         <Text style={{ margin: 32, letterSpacing: 0.98 }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -335,48 +152,7 @@ export default function DetailsPage() {
           }}
         />
 
-        <View style={{ margin: 32 }}>
-          <Text style={{ fontSize: 20, fontWeight: 500 }}>
-            Où vous dormirez
-          </Text>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-around",
-              alignItems: "center",
-              marginTop: 24,
-            }}
-          >
-            <View>
-              <Image
-                source={{ uri: listing.image }}
-                placeholder={{ blurhash }}
-                contentFit="cover"
-                transition={1000}
-                style={{
-                  height: 100,
-                  width: 160,
-                  borderRadius: 16,
-                }}
-              />
-              <Text style={{ marginTop: 6 }}>Chambre 1</Text>
-            </View>
-            <View>
-              <Image
-                source={{ uri: listing.image }}
-                placeholder={{ blurhash }}
-                contentFit="cover"
-                transition={1000}
-                style={{
-                  height: 100,
-                  width: 160,
-                  borderRadius: 16,
-                }}
-              />
-              <Text style={{ marginTop: 6 }}>Chambre 2</Text>
-            </View>
-          </View>
-        </View>
+        <WhereYouSleep listing={listing} />
 
         <View
           style={{
@@ -385,57 +161,7 @@ export default function DetailsPage() {
           }}
         />
 
-        <View style={{ margin: 32, gap: 16 }}>
-          <Text style={{ fontSize: 20, fontWeight: 500 }}>
-            Ce que la place offre
-          </Text>
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
-            <MaterialCommunityIcons
-              name="silverware-fork-knife"
-              size={24}
-              color="black"
-            />
-            <Text style={{ fontSize: 16, fontWeight: 300 }}>Cuisine</Text>
-          </View>
-
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
-            <FontAwesome5 name="wifi" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: 300 }}>Wifi</Text>
-          </View>
-
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
-            <MaterialCommunityIcons name="desk" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: 300 }}>Bureau</Text>
-          </View>
-
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
-            <FontAwesome5 name="car" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: 300 }}>
-              Parking gratuit
-            </Text>
-          </View>
-
-          <View style={{ flexDirection: "row", alignItems: "center", gap: 20 }}>
-            <FontAwesome5 name="camera" size={24} color="black" />
-            <Text style={{ fontSize: 16, fontWeight: 300 }}>
-              Sécurité renforcée
-            </Text>
-          </View>
-
-          <Pressable
-            style={({ pressed }) => ({
-              borderWidth: 0.5,
-              paddingVertical: 16,
-              borderRadius: 6,
-              marginTop: 20,
-              opacity: pressed ? 0.6 : 1,
-            })}
-          >
-            <Text style={{ alignSelf: "center" }}>
-              Montrer les 55 commodités
-            </Text>
-          </Pressable>
-        </View>
+        <WhatItOffers />
 
         <View
           style={{
@@ -444,16 +170,7 @@ export default function DetailsPage() {
           }}
         />
 
-        <View style={{ margin: 32, gap: 16 }}>
-          <Text style={{ fontSize: 20, fontWeight: 500 }}>Où vous serez</Text>
-          <Text>
-            {listing.city}, {listing.country}
-          </Text>
-
-          <View style={{ borderRadius: 20 }}>
-            <AppleGoogleMap styling={{ height: 300, width: "100%" }} />
-          </View>
-        </View>
+        <DisplayMap listing={listing} />
 
         <View
           style={{
@@ -462,107 +179,10 @@ export default function DetailsPage() {
           }}
         />
 
-        <View
-          style={{
-            position: "relative",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            paddingVertical: 20,
-          }}
-        >
-          <LinearGradient
-            colors={["#F7F7F7", "#FEFEFE"]}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              top: 0,
-              height: "auto",
-            }}
-          >
-            <View
-              style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: 26,
-                marginTop: 26,
-              }}
-            >
-              <FontAwesome5
-                name="leaf"
-                size={50}
-                color="black"
-                style={{ transform: [{ scaleX: -1 }] }}
-              />
-
-              <Text style={{ fontSize: 60, fontWeight: 500 }}>
-                {listing.rating}
-              </Text>
-
-              <FontAwesome5 name="leaf" size={50} color="black" />
-            </View>
-          </LinearGradient>
-        </View>
+        <GuestFavorite listing={listing} />
       </Animated.ScrollView>
 
-      {/* Footer Btn */}
-      {/* <View */}
-      {/*   style={{ */}
-      {/*     position: "absolute", */}
-      {/*     bottom: 0, */}
-      {/*     left: 0, */}
-      {/*     flexDirection: "row", */}
-      {/*     justifyContent: "space-between", */}
-      {/*     alignItems: "center", */}
-      {/*     paddingHorizontal: 24, */}
-      {/*     paddingVertical: 16, */}
-      {/*     borderTopWidth: 0.5, */}
-      {/*     width: "100%", */}
-      {/*     backgroundColor: "white", */}
-      {/*   }} */}
-      {/* > */}
-      {/*   <View style={{ marginVertical: 14 }}> */}
-      {/*     <Text */}
-      {/*       style={{ */}
-      {/*         fontWeight: 500, */}
-      {/*         fontSize: 18, */}
-      {/*         textDecorationLine: "underline", */}
-      {/*       }} */}
-      {/*     > */}
-      {/*       {listing.price} € / nuit */}
-      {/*     </Text> */}
-      {/*     <View */}
-      {/*       style={{ */}
-      {/*         flexDirection: "row", */}
-      {/*         alignItems: "center", */}
-      {/*         gap: 10, */}
-      {/*         paddingHorizontal: 10, */}
-      {/*         paddingVertical: 4, */}
-      {/*         backgroundColor: "#E5E5E5", */}
-      {/*         borderRadius: 999, */}
-      {/*         marginTop: 4, */}
-      {/*       }} */}
-      {/*     > */}
-      {/*       <Ionicons name="checkmark-outline" size={16} color={"black"} /> */}
-      {/*       <Text>Annulation gratuite</Text> */}
-      {/*     </View> */}
-      {/*   </View> */}
-      {/*   <Pressable */}
-      {/*     style={({ pressed }) => ({ */}
-      {/*       backgroundColor: Colors.primary, */}
-      {/*       paddingVertical: 14, */}
-      {/*       paddingHorizontal: 44, */}
-      {/*       borderRadius: 8, */}
-      {/*       opacity: pressed ? 0.6 : 1, */}
-      {/*     })} */}
-      {/*   > */}
-      {/*     <Text style={{ color: "white", fontWeight: 600, fontSize: 16 }}> */}
-      {/*       Réserver */}
-      {/*     </Text> */}
-      {/*   </Pressable> */}
-      {/* </View> */}
+      <FooterBtn listing={listing} />
     </View>
   );
 }
