d326f6e11fb37c7e53ddcc689a5d5a91038def97
Delgorithm <delgorithm@gmail.com>
2025-05-21 16:29:36 +0200

    Auth : Clerk on its way

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,22 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import "react-native-reanimated";
 import { useEffect } from "react";
+import { ClerkProvider } from "@clerk/clerk-expo";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error("Add Publishable Key");
+  }
+
+  const [loaded] = useFonts({
     mon: require("../assets/fonts/Montserrat-Regular.ttf"),
     "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
     "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
-
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
 
   useEffect(() => {
     if (loaded) {
@@ -19,14 +25,13 @@ export default function RootLayout() {
     }
   }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(modals)" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey}>
+      <Stack>
+        <Stack.Screen name="(modals)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
+    </ClerkProvider>
   );
 }
