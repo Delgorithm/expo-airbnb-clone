fa4b45170213e3b0e4646b8c69e8279022bc5939
Delgorithm <delgorithm@gmail.com>
2025-05-26 00:07:26 +0200

    Component : Modal for the cancellation

--- app/(modals)/cancellation/[cancellationModal].tsx
+++ app/(modals)/cancellation/[cancellationModal].tsx
@@ -1,9 +1,59 @@
-import { Text, View } from "react-native";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { Pressable, Text, View } from "react-native";
+import * as SQLite from "expo-sqlite";
+import { eq } from "drizzle-orm";
+import { reservations } from "@/db/schema";
+import { useEffect, useState } from "react";
+import { router, useLocalSearchParams } from "expo-router";
+
+const db = drizzle(SQLite.openDatabaseSync("db.db"));
 
 export default function CancellationModal() {
+  const [hasReservation, setHasReservation] = useState<null | {
+    listingId: number;
+  }>(null);
+  const { cancellationModal } = useLocalSearchParams();
+  const listingCurrentId = cancellationModal;
+
+  useEffect(() => {
+    const fetchReservation = async () => {
+      const result = db
+        .select({
+          listingId: reservations.listingId,
+        })
+        .from(reservations)
+        .where(eq(reservations.listingId, Number(listingCurrentId)))
+        .limit(1)
+        .get();
+
+      if (result) {
+        setHasReservation(result);
+      }
+    };
+    fetchReservation();
+  }, [listingCurrentId]);
+
+  const handleCancelReservation = async () => {
+    db.delete(reservations)
+      .where(eq(reservations.listingId, Number(listingCurrentId)))
+      .run();
+
+    router.back();
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Cancellation</Text>
+      <Pressable
+        onPress={handleCancelReservation}
+        style={{
+          paddingVertical: 10,
+          paddingHorizontal: 16,
+          backgroundColor: "black",
+        }}
+      >
+        <Text style={{ color: "white" }}>Annuler la réservation</Text>
+      </Pressable>
     </View>
   );
 }
