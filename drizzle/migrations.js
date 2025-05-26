c835d4ed2ba69387ef946e55329a7b177503d581
Delgorithm <delgorithm@gmail.com>
2025-05-26 14:56:18 +0200

    db : migration

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_sleepy_gamora.sql';
 import m0001 from './0001_melted_captain_america.sql';
+import m0002 from './0002_lean_firedrake.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
